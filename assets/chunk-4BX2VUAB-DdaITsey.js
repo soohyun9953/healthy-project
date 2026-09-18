bf28bcff49557ed6f944d7d599c40b0ae9f9231c
SooHyun Lee <soohyun9953@gmail.com>
2026-09-18 14:32:56 +0900

    Updates

--- assets/chunk-4BX2VUAB-DdaITsey.js
+++
@@ -1,1 +0,0 @@
-import{_ as i}from"./index-DQ4rt1Ln.js";function t(c,e){c.accDescr&&e.setAccDescription?.(c.accDescr),c.accTitle&&e.setAccTitle?.(c.accTitle),c.title&&e.setDiagramTitle?.(c.title)}i(t,"populateCommonDb");export{t as p};
